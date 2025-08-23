dbff089bd4ca831bee18d11707d262928783afca
Alberto Lazari <alberto.lazari@gmail.com>
2025-08-23 18:22:39 +0200

    Gradient color for paddle

--- src/Paddle.cpp
+++ src/Paddle.cpp
@@ -14,6 +14,9 @@ Paddle::Paddle(float x, float y)
             GL_VERTEX_SHADER, R"glsl(
                 #version 330 core
                 layout(location = 0) in vec2 aPos;
+                layout(location = 1) in vec3 aColor;
+                out vec3 vColor;
+
                 uniform vec2 uScale;
                 uniform vec2 uOffset;
                 uniform float uAspect;
@@ -23,17 +26,20 @@ Paddle::Paddle(float x, float y)
                     vec2 scaledPos = aPos * uScale;
                     scaledPos.x /= uAspect;
                     gl_Position = vec4(scaledPos + uOffset, 0.0, 1.0);
+
+                    vColor = aColor;
                 }
             )glsl"
         },
         {
             GL_FRAGMENT_SHADER, R"glsl(
                 #version 330 core
-                out vec4 FragColor;
+                in vec3 vColor;
+                out vec4 fragColor;
 
                 void main()
                 {
-                    FragColor = vec4(0.7, 0.7, 0.7, 1.0);
+                    fragColor = vec4(vColor, 1.0);
                 }
             )glsl"
         },
@@ -71,15 +77,39 @@ void Paddle::initBuffers()
     glGenVertexArrays(1, &vaoHandle);
     glBindVertexArray(vaoHandle);
 
-    float vertices[] = {
-        -0.5f, -0.5f,
-         0.5f, -0.5f,
-         0.5f,  0.5f,
-        -0.5f,  0.5f,
+    constexpr unsigned int positionAttributes = 2;
+    constexpr unsigned int colorAttributes = 3;
+    constexpr unsigned int attributes = positionAttributes + colorAttributes;
+    float vertices[][attributes] = {
+        // posX,  posY,     r,    g,    b
+        { -0.5f, -0.5f,     0.4f, 0.4f, 0.4f },
+        {  0.5f, -0.5f,     0.4f, 0.4f, 0.4f },
+        {  0.5f,  0.5f,     0.9f, 0.9f, 0.9f },
+        { -0.5f,  0.5f,     0.9f, 0.9f, 0.9f },
     };
     glGenBuffers(1, &vboHandle);
     glBindBuffer(GL_ARRAY_BUFFER, vboHandle);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Position attribute
+    glVertexAttribPointer(
+        /* location */              0,
+        /* number of elements */    positionAttributes,
+        /* type of elements */      GL_FLOAT,
+        /* normalize */             GL_FALSE,
+        /* stride (vertex size) */  sizeof(*vertices),
+        /* attribute offset */      (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Color attribute
+    glVertexAttribPointer(
+        /* location */              1,
+        /* number of elements */    colorAttributes,
+        /* type of elements */      GL_FLOAT,
+        /* normalize */             GL_FALSE,
+        /* stride (vertex size) */  sizeof(*vertices),
+        /* attribute offset */      (void*)(positionAttributes * sizeof(**vertices)));
+    glEnableVertexAttribArray(1);
 
     unsigned int indices[] = {
         0, 1, 2,
@@ -89,9 +119,7 @@ void Paddle::initBuffers()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboHandle);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
-    glEnableVertexAttribArray(0);
-
+    // Unbind vbo and vao, to allow potential reuse without conflicts
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
