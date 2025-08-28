ac2ed2f54f8d08f8b5444677f400e8e19e5f53ee
Alberto Lazari <alberto.lazari@gmail.com>
2025-08-28 12:48:35 +0200

    Comment

--- src/BrickMap.h
+++ src/BrickMap.h
@@ -18,7 +18,15 @@ public:
     void update(float dt);
     void render(float aspectRatio, float alpha);
 
+    /**
+     * Binary search up to four bricks which are nearest to the quad.
+     * In the general case, the four bricks are { bottom-right, bottom-left, top-right, top-left }.
+     * Bricks are guaranteed to be returned in the order shown above, which is the opposite of the
+     * ordering of rows and columns. This way, bricks can safely be destroyed, without invalidating
+     * next iterators.
+     */
     std::vector<std::pair<Rows::iterator, Columns::iterator>> findBricksNearby(const Quad& quad);
+
     void destroyBrick(const Rows::iterator& rowIt, const Columns::iterator& brickIt);
 
 private:
